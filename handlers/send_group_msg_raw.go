201530d66314d9b0c23f60ac05dd84a449589326
yuelengqingchen <2279839557@qq.com>
2026-08-02 00:26:11 +0800

    test

--- handlers/send_group_msg_raw.go
+++ handlers/send_group_msg_raw.go
@@ -227,6 +227,7 @@ func HandleSendGroupMsgRaw(client callapi.Client, api openapi.OpenAPI, apiv2 ope
 
 		if imageCount == 1 && messageText != "" {
 			mylog.Printf("发图文混合信息-群")
+			mylog.Printf("test——raw")
 			// 创建包含单个图片的 singleItem
 			singleItem[imageType] = []string{imageUrl}
 			msgseq := echo.GetMappingSeq(messageID)
