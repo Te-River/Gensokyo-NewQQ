975cccc2d50e7d08fc4d345adab621128282dd5e
yuelengqingchen <2279839557@qq.com>
2026-08-02 00:05:48 +0800

    test

--- handlers/send_group_msg.go
+++ handlers/send_group_msg.go
@@ -368,7 +368,9 @@ func HandleSendGroupMsg(client callapi.Client, api openapi.OpenAPI, apiv2 openap
 			  }
 			  // 图文混合消息同样需要转换 [CQ:at] 为 @用户名，与纯文本路径对齐
 			  // 否则 QQ 官方 API 不识别 CQ 码，会原文显示 [CQ:at,qq=数字]
+		      mylog.Println("CQat转换前:", messageText)
 			  messageText = resolvePlainTextAtMentions(messageText)
+			  mylog.Println("CQat转换后:", messageText)
 			  // 创建包含文本和图像信息的消息
 			  msgseq = echo.GetMappingSeq(messageID)
 			  echo.AddMappingSeq(messageID, msgseq+1)
